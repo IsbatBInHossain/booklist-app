0c257c8995d9d6fe57cc61c7d848ef439e182bce
Isbat-Bin-Hossain <isbat31416@gmail.com>
2023-03-26 13:41:05 +0600

    Complete v2.0 Add external api to store data

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
+import axios from "axios";
+const URL = "http://localhost:5000/books/";
 
 const App = () => {
   const [books, setBooks] = useState([]);
-  const handleCreateBook = (title) => {
-    setBooks([...books, { id: Math.floor(Math.random() * 9999), title }]);
+  const fetchBooks = () =>
+    axios.get(URL).then((response) => setBooks(response.data));
+  useEffect(() => {
+    fetchBooks();
+  }, []);
+  const handleCreateBook = async (title) => {
+    // setBooks([...books, { id: Math.floor(Math.random() * 9999), title }]);
+    const response = await axios.post(URL, {
+      title,
+    });
+    setBooks([...books, response.data]);
   };
-  const deleteBooksById = (id) => {
+  const deleteBooksById = async (id) => {
+    await axios.delete(`${URL}${id}`);
     const updatedBooks = books.filter((book) => {
       return book.id !== id;
     });
     setBooks(updatedBooks);
   };
 
-  const handleEdit = (id, newTitle) => {
+  const handleEdit = async (id, newTitle) => {
+    const response = await axios.put(`${URL}${id}`, {
+      title: newTitle,
+    });
     const updatedBooks = books.map((book) => {
       if (book.id === id) {
-        return { ...book, title: newTitle };
+        return { ...book, ...response.data };
       }
       return book;
     });
